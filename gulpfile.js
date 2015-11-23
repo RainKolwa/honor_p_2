39c85d0a8c7815757025d944058d172b22603c75
RainKolwa <jeremy199003@gmail.com>
2015-11-23 17:01:32 +0800

    引入art-template

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var browserSync = require('browser-sync').create();
 var paths = {
 	scripts: [
 		'node_modules/jquery/dist/jquery.min.js',
+		'node_modules/art-template/dist/template.js',
 		'node_modules/gsap/src/minified/TweenMax.min.js'
 	],
 	styles: [
--- js/page.js
+++ js/page.js
@@ -1,19 +1,17 @@
-(function ($, TL, TM) {
+(function ($, TL, TM, template) {
 	var HONOR = {};
 
 	// 入场动画
 	HONOR.Landing = (function(){
 
-		var text = " 他们有着独特的个性、出众的外表，拥有荣耀星人的特质，他们是我们的明星战队！ \
-		在荣耀2周年狂欢Party现场，你们可以见到他们本人哦！ \
-		让我们一起来猜一下，分别都有谁？",
+		var text = " 他们有着独特的个性、出众的外表，拥有荣耀星人的特质，他们是我们的明星战队！在荣耀2周年狂欢Party现场，你们可以见到他们本人哦！让我们一起来猜一下，分别都有谁？",
 			delay = 0,
 			currentChar = 1;
 
 		var init = function(){
 			// startTyping(text, 50, "test");
 			showPlanet();
-		}
+		};
 
 		var type = function(){
 			var dest=document.getElementById(destination);
@@ -25,12 +23,12 @@
 				{
 					setTimeout(type, delay);
 				}else{
-					console.log("complete")
+					console.log("complete");
 					// 显示星球
 					showPlanet();
 				}
 			}	
-		}
+		};
 
 		var startTyping = function(textParam, delayParam, destinationParam){
 			text=textParam;
@@ -38,21 +36,21 @@
 			currentChar=1;
 			destination=destinationParam;
 			type();
-		}
+		};
 
 		var showPlanet = function(){
 			var planet1 = $('.planet-1');
 			var planet2 = $('.planet-2');
-			TL.from(planet1, 5, {left: '50%', top: '0%', scale: .1, opacity: .5, onComplete: completePlanetHandler})
-			TL.from(planet2, 5, {left: '40%', top: '45%', scale: .3, opacity: .5})
-		}
+			TL.from(planet1, 5, {left: '50%', top: '0%', scale: 0.1, opacity: 0.5, onComplete: completePlanetHandler});
+			TL.from(planet2, 5, {left: '40%', top: '45%', scale: 0.3, opacity: 0.5});
+		};
 
 		var completePlanetHandler = function(){
 			// 显示猜题入口
-			console.log('start')
+			console.log('start');
 			HONOR.Panel.init();
 			HONOR.Panel.setStatus('start');
-		}
+		};
 		
 		return {
 			init: init
@@ -67,7 +65,7 @@
 		var init = function(){
 
 			bindEvents();
-		}
+		};
 
 		var bindEvents = function(){
 			statusBox.on('click', 'a', function(e){
@@ -81,8 +79,8 @@
 				}else{
 					return;
 				}
-			})
-		}
+			});
+		};
 
 		var setStatus = function(type){
 			switch (type){
@@ -98,12 +96,12 @@
 				default:
 					statusBox.find('.default').show().siblings().hide();
 			}
-		}
+		};
 
 		return {
 			init: init,
 			setStatus: setStatus
-		}
+		};
 	})();
 
 	// 抽题
@@ -111,8 +109,8 @@
 
 		var questions = [
 			{
+				img: "images/1.jpg",
 				desc: "哈哈",
-				imgSrc: "",
 				content: ["你","好","吗","停","之","哈","奇","葩","舒","服","撒","发","生","啊","是","分","手"]
 			},
 			{
@@ -143,49 +141,36 @@
 		var numberPerLine = 7;
 
 		var examBox = $('.examBox');
-		var 
-		var selectionBox = examBox.find('.selections');
 
 		var init = function(){
 			bindEvents();
-		}
+		};
 
 		var bindEvents = function(){
 
-		}
+		};
 
 		var randomQuestionIndex = function(){
 			// 随机产生[0,7]之间的整数
 			return Math.floor(Math.random() * questions.length);
-		}
+		};
 
 		var showQuestion = function(index){
 			// 显示第 index 条题目
-			var content = questions[index].content; // array
-			var desc = questions[index].desc;
-			var questionHTML = '';
-			for(var i = 0;i++;i > content.length){
-				var left = 100 * i;
-				var top = 0;
-				if(i > numberPerLine){
-					top = (i/numberPerLine>2)? 80 : 40;
-				}
-				var style = "position:absolute;left:"+left+"px;top:"+top+"px;";
-				questionHTML += "<a style="+style+">"+content[i]+"</a>";
-			}
-			$('.dom').html(questionHTML);
-			$('.dom').show();
-		}
+			var html = template('examView', questions[index]);
+			$('#examBox').html(html);
+		};
 
 		var randomQuestion = function(){
 			var index = randomQuestionIndex();
 			showQuestion(index);
-		}
+		};
 
 		return {
 			init: init,
-			randomQuestion: randomQuestion
-		}
+			randomQuestion: randomQuestion,
+			showQuestion: showQuestion
+		};
 	})();
 
 	// 答题结果
@@ -193,18 +178,17 @@
 		
 		var show = function(){
 
-		}
+		};
 
 		return {
 
-		}
+		};
 	})();
 
 	// 启动飞船
 	HONOR.Landing.init();
+	HONOR.Exam.showQuestion(0);
 
-})(jQuery, TweenLite, TweenMax);
-
-
+})(jQuery, TweenLite, TweenMax, template);
 
 
