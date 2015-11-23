9677c36270cfecd0f9f7d58d969b7d73b337569a
RainKolwa <jeremy199003@gmail.com>
2015-11-23 15:47:21 +0800

    逻辑框架

--- js/page.js
+++ js/page.js
@@ -27,7 +27,7 @@
 				}else{
 					console.log("complete")
 					// 显示星球
-
+					showPlanet();
 				}
 			}	
 		}
@@ -50,6 +50,8 @@
 		var completePlanetHandler = function(){
 			// 显示猜题入口
 			console.log('start')
+			HONOR.Panel.init();
+			HONOR.Panel.setStatus('start');
 		}
 		
 		return {
@@ -59,12 +61,142 @@
 
 	// 控制面板
 	HONOR.Panel = (function(){
+		var panel = $('.panel'),
+			statusBox = panel.find('.statusBox');
+
 		var init = function(){
+
+			bindEvents();
+		}
+
+		var bindEvents = function(){
+			statusBox.on('click', 'a', function(e){
+				e.preventDefault();
+				if($(this).hasClass('start')){
+
+				}else if($(this).hasClass('next')){
+
+				}else if($(this).hasClass('submit')){
+
+				}else{
+					return;
+				}
+			})
+		}
+
+		var setStatus = function(type){
+			switch (type){
+				case 'start':
+					statusBox.find('.start').show().siblings().hide();
+					break;
+				case 'next':
+					statusBox.find('.next').show().siblings().hide();
+					break;
+				case 'submit':
+					statusBox.find('.submit').show().siblings().hide();
+					break;
+				default:
+					statusBox.find('.default').show().siblings().hide();
+			}
+		}
+
+		return {
+			init: init,
+			setStatus: setStatus
+		}
+	})();
+
+	// 抽题
+	HONOR.Exam = (function(){
+
+		var questions = [
+			{
+				desc: "哈哈",
+				imgSrc: "",
+				content: ["你","好","吗","停","之","哈","奇","葩","舒","服","撒","发","生","啊","是","分","手"]
+			},
+			{
+				content: ["你","好","吗","停","之","哈","奇","葩","舒","服","撒","发","生","啊","是","分","手"]
+			},
+			{
+				content: ["你","好","吗","停","之","哈","奇","葩","舒","服","撒","发","生","啊","是","分","手"]
+			},
+			{
+				content: ["你","好","吗","停","之","哈","奇","葩","舒","服","撒","发","生","啊","是","分","手"]
+			},
+			{
+				content: ["你","好","吗","停","之","哈","奇","葩","舒","服","撒","发","生","啊","是","分","手"]
+			},
+			{
+				content: ["你","好","吗","停","之","哈","奇","葩","舒","服","撒","发","生","啊","是","分","手"]
+			},
+			{
+				content: ["你","好","吗","停","之","哈","奇","葩","舒","服","撒","发","生","啊","是","分","手"]
+			},
+			{
+				content: ["你","好","吗","停","之","哈","奇","葩","舒","服","撒","发","生","啊","是","分","手"]
+			}
+		];
+
+		var examTotal = 3; // 随机题数
+		var answeredCount = 0;
+		var numberPerLine = 7;
+
+		var examBox = $('.examBox');
+		var 
+		var selectionBox = examBox.find('.selections');
+
+		var init = function(){
+			bindEvents();
+		}
+
+		var bindEvents = function(){
+
+		}
+
+		var randomQuestionIndex = function(){
+			// 随机产生[0,7]之间的整数
+			return Math.floor(Math.random() * questions.length);
+		}
+
+		var showQuestion = function(index){
+			// 显示第 index 条题目
+			var content = questions[index].content; // array
+			var desc = questions[index].desc;
+			var questionHTML = '';
+			for(var i = 0;i++;i > content.length){
+				var left = 100 * i;
+				var top = 0;
+				if(i > numberPerLine){
+					top = (i/numberPerLine>2)? 80 : 40;
+				}
+				var style = "position:absolute;left:"+left+"px;top:"+top+"px;";
+				questionHTML += "<a style="+style+">"+content[i]+"</a>";
+			}
+			$('.dom').html(questionHTML);
+			$('.dom').show();
+		}
+
+		var randomQuestion = function(){
+			var index = randomQuestionIndex();
+			showQuestion(index);
+		}
+
+		return {
+			init: init,
+			randomQuestion: randomQuestion
+		}
+	})();
+
+	// 答题结果
+	HONOR.Result = (function(){
+		
+		var show = function(){
 
 		}
 
 		return {
-			init: init
+
 		}
 	})();
 
